3df98bfd1b2535e150e5be44656dbfc71ebc7925
davidteather <34144122+davidteather@users.noreply.github.com>
2024-04-07 15:39:33 -0500

    fix: the file upload prcoess by defining everything in single_file.py only edit that one

---
+++ single_file.py
@@ -0,0 +1,515 @@
+import json
+from json import JSONEncoder
+import numpy as np
+from typing import List, Tuple, Dict
+
+
+
+"""
+ Data Model
+"""
+
+Time = int
+Symbol = str
+Product = str
+Position = int
+UserId = str
+Observation = int
+
+
+class Listing:
+    def __init__(self, symbol: Symbol, product: Product, denomination: Product):
+        self.symbol = symbol
+        self.product = product
+        self.denomination = denomination
+
+
+class Order:
+    def __init__(self, symbol: Symbol, price: int, quantity: int) -> None:
+        self.symbol = symbol
+        self.price = price
+        self.quantity = quantity
+
+    def __str__(self) -> str:
+        return "(" + self.symbol + ", " + str(self.price) + ", " + str(self.quantity) + ")"
+
+    def __repr__(self) -> str:
+        return "(" + self.symbol + ", " + str(self.price) + ", " + str(self.quantity) + ")"
+
+
+class OrderDepth:
+    def __init__(self, buy_orders=None, sell_orders=None):
+        if sell_orders is None:
+            sell_orders = {}
+        if buy_orders is None:
+            buy_orders = {}
+        self.buy_orders: Dict[int, int] = buy_orders
+        self.sell_orders: Dict[int, int] = sell_orders
+
+
+class Trade:
+    def __init__(self, symbol: Symbol, price: int, quantity: int, buyer: UserId = None, seller: UserId = None,
+                 timestamp: int = 0) -> None:
+        self.symbol = symbol
+        self.price: int = price
+        self.quantity: int = quantity
+        self.buyer = buyer
+        self.seller = seller
+        self.timestamp = timestamp
+
+class TradingState(object):
+    def __init__(self,
+                 traderData: str,
+                 timestamp: int,
+                 listings: Dict[str, Listing],
+                 order_depths: Dict[str, OrderDepth],
+                 own_trades: Dict[str, List[Trade]],
+                 market_trades: Dict[str, List[Trade]],
+                 position: Dict[str, int],
+                 observations: Observation):
+        self.traderData = traderData
+        self.timestamp = timestamp
+        self.listings = listings
+        self.order_depths = order_depths
+        self.own_trades = own_trades
+        self.market_trades = market_trades
+        self.position = position
+        self.observations = observations
+        
+    def toJSON(self):
+       return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True)
+
+
+class ProsperityEncoder(JSONEncoder):
+    def default(self, o):
+        return o.__dict__
+
+"""
+Strategies
+"""
+
+class Strategy:
+    def __init__(self, name: str, max_position: int):
+        self.name: str = name
+        self.cached_prices: list = []
+        self.cached_means: list = []
+        self.max_pos: int = max_position
+        self.trade_count: int = 1
+
+        self.prod_position: int = 0
+        self.new_buy_orders: int = 0
+        self.new_sell_orders: int = 0
+        self.order_depth: OrderDepth = OrderDepth()
+
+    def reset_from_state(self, state: TradingState):
+        self.prod_position = state.position[self.name] if self.name in state.position.keys() else 0
+        self.order_depth: OrderDepth = state.order_depths[self.name]
+
+        self.new_buy_orders = 0
+        self.new_sell_orders = 0
+
+    def sell_product(self, best_bids, i, order_depth, orders):
+        # Sell product at best bid
+        best_bid_volume = order_depth.buy_orders[best_bids[i]]
+        if self.prod_position - best_bid_volume >= -self.max_pos:
+            orders.append(Order(self.name, best_bids[i], -best_bid_volume))
+            self.prod_position += -best_bid_volume
+            self.new_sell_orders += best_bid_volume
+
+        else:
+            # Sell as much as we can without exceeding the self.max_pos[product]
+            vol = self.prod_position + self.max_pos
+            orders.append(Order(self.name, best_bids[i], -vol))
+            self.prod_position += -vol
+            self.new_sell_orders += vol
+
+    def buy_product(self, best_asks, i, order_depth, orders):
+        # Buy product at best ask
+        best_ask_volume = order_depth.sell_orders[best_asks[i]]
+        if self.prod_position - best_ask_volume <= self.max_pos:
+            orders.append(Order(self.name, best_asks[i], -best_ask_volume))
+            self.prod_position += -best_ask_volume
+            self.new_buy_orders += -best_ask_volume
+        else:
+            # Buy as much as we can without exceeding the self.max_pos[product]
+            vol = self.max_pos - self.prod_position
+            orders.append(Order(self.name, best_asks[i], vol))
+            self.prod_position += vol
+            self.new_buy_orders += vol
+
+    def continuous_buy(self, order_depth: OrderDepth, orders: list):
+        if len(order_depth.sell_orders) != 0:
+            best_asks = sorted(order_depth.sell_orders.keys())
+
+            i = 0
+            while i < self.trade_count and len(best_asks) > i:
+                if self.prod_position == self.max_pos:
+                    break
+
+                self.buy_product(best_asks, i, order_depth, orders)
+                i += 1
+
+    def continuous_sell(self, order_depth: OrderDepth, orders: list):
+        if len(order_depth.buy_orders) != 0:
+            best_bids = sorted(order_depth.buy_orders.keys(), reverse=True)
+
+            i = 0
+            while i < self.trade_count and len(best_bids) > i:
+                if self.prod_position == -self.max_pos:
+                    break
+
+                self.sell_product(best_bids, i, order_depth, orders)
+                i += 1
+
+
+class CrossStrategy(Strategy):
+    def __init__(self, name: str, min_req_price_difference: int, max_position: int):
+        super().__init__(name, max_position)
+        self.strategy_start_day = 2
+
+        self.old_asks = []
+        self.old_bids = []
+        self.min_req_price_difference = min_req_price_difference
+
+    def trade(self, trading_state: TradingState, orders: list):
+        order_depth: OrderDepth = trading_state.order_depths[self.name]
+        self.cache_prices(order_depth)
+        if len(self.old_asks) < self.strategy_start_day or len(self.old_bids) < self.strategy_start_day:
+            return
+
+        avg_bid, avg_ask = self.calculate_prices(self.strategy_start_day)
+
+        if len(order_depth.sell_orders) != 0:
+            best_asks = sorted(order_depth.sell_orders.keys())
+
+            i = 0
+            while i < self.trade_count and len(best_asks) > i and best_asks[i] - avg_bid <= self.min_req_price_difference:
+                if self.prod_position == self.max_pos:
+                    break
+                self.buy_product(best_asks, i, order_depth, orders)
+                i += 1
+
+        if len(order_depth.buy_orders) != 0:
+            best_bids = sorted(order_depth.buy_orders.keys(), reverse=True)
+
+            i = 0
+            while i < self.trade_count and len(best_bids) > i and avg_ask - best_bids[i] <= self.min_req_price_difference:
+                if self.prod_position == -self.max_pos:
+                    break
+                self.sell_product(best_bids, i, order_depth, orders)
+
+                i += 1
+
+    def calculate_prices(self, days: int) -> Tuple[int, int]:
+        # Calculate the average bid and ask price for the last days
+
+        relevant_bids = []
+        for bids in self.old_bids[-days:]:
+            relevant_bids.extend([(value, bids[value]) for value in bids])
+        relevant_asks = []
+        for asks in self.old_asks[-days:]:
+            relevant_asks.extend([(value, asks[value]) for value in asks])
+
+        avg_bid = np.average([x[0] for x in relevant_bids], weights=[x[1] for x in relevant_bids])
+        avg_ask = np.average([x[0] for x in relevant_asks], weights=[x[1] for x in relevant_asks])
+
+        return avg_bid, avg_ask
+
+    def cache_prices(self, order_depth: OrderDepth):
+        sell_orders = order_depth.sell_orders
+        buy_orders = order_depth.buy_orders
+
+        self.old_asks.append(sell_orders)
+        self.old_bids.append(buy_orders)
+
+
+class DiffStrategy(Strategy):
+    def __init__(self, name: str, max_position: int, derivative_resolution: int, diff_thresh: int):
+        super().__init__(name, max_position)
+        self.derivative_resolution: int = derivative_resolution
+        self.diff_thresh: int = diff_thresh
+
+    def trade(self, trading_state: TradingState, orders: list):
+        order_depth: OrderDepth = trading_state.order_depths[self.name]
+        self.cache_purchased_prices(trading_state)
+        self.calculate_means()
+
+        diff = self.get_price_difference()
+
+        if diff < -self.diff_thresh and len(order_depth.sell_orders) != 0:
+            self.continuous_buy(order_depth, orders)
+
+        if diff > self.diff_thresh and len(order_depth.buy_orders) != 0:
+            self.continuous_sell(order_depth, orders)
+
+    def get_price_difference(self) -> float:
+        # Calculate the difference between the current mean and the mean from
+        # self.derivative_resolution days ago
+        if len(self.cached_means) < self.derivative_resolution + 1:
+            old_mean = self.cached_means[0]
+        else:
+            old_mean = self.cached_means[-self.derivative_resolution]
+        diff = self.cached_means[-1] - old_mean
+        return diff
+
+    def calculate_means(self):
+        #
+        if len(self.cached_prices) == 0:
+            self.cached_means.append(0)
+
+        else:
+            relevant_prices = []
+            for day_prices in self.cached_prices[max(-len(self.cached_prices), -1):]:
+                for price in day_prices:
+                    relevant_prices.append(price)
+            prices = np.array([x[1] for x in relevant_prices])
+            quantities = np.abs(np.array([x[0] for x in relevant_prices]))
+
+            self.cached_means.append(np.average(prices, weights=quantities))
+
+    def cache_purchased_prices(self, state: TradingState) -> None:
+        # Caches prices of bought and sold products
+
+        market_trades = state.market_trades
+        own_trades = state.own_trades
+
+        prod_trades: List[Trade] = own_trades.get(self.name, []) + market_trades.get(self.name, [])
+
+        if len(prod_trades) > 0:
+            prices = [(trade.quantity, trade.price) for trade in prod_trades]
+            self.cached_prices.append(prices)
+
+
+class FixedStrategy(Strategy):
+    def __init__(self, name: str, max_pos: int):
+        super().__init__(name, max_pos)
+        self.amethyst_price = 10000 # TODO: verify
+        self.amethyst_diff = 4
+
+    def trade(self, trading_state: TradingState, orders: list):
+        order_depth: OrderDepth = trading_state.order_depths[self.name]
+        # Check if there are any SELL orders
+        if len(order_depth.sell_orders) > 0:
+            #
+            # self.cache_prices(order_depth)
+            # Sort all the available sell orders by their price
+            best_asks = sorted(order_depth.sell_orders.keys())
+
+            # Check if the lowest ask (sell order) is lower than the above defined fair value
+            i = 0
+            while i < self.trade_count and best_asks[i] < self.amethyst_price:
+                # Fill ith ask order if it's below the acceptable
+                if self.prod_position == self.max_pos:
+                    break
+                self.buy_product(best_asks, i, order_depth, orders)
+                i += 1
+        if len(order_depth.buy_orders) != 0:
+            best_bids = sorted(order_depth.buy_orders.keys(), reverse=True)
+
+            i = 0
+            while i < self.trade_count and best_bids[i] > self.amethyst_price:
+                if self.prod_position == -self.max_pos:
+                    break
+                self.sell_product(best_bids, i, order_depth, orders)
+                i += 1
+
+
+class ObservationStrategy(Strategy):
+    def __init__(self, name: str, max_position: int):
+        super().__init__(name, max_position)
+        self.old_dolphins = -1
+        self.dolphins_spotted = False
+        self.dolphins_gone = False
+
+        self.dolphin_action_time = 900
+        self.gear_timestamp_diff = 70000
+
+        self.dolphins_spotted_timestamp = -1
+        self.dolphins_gone_timestamp = -1
+
+    def handle_observations(self, trading_state: TradingState):
+        for observation in trading_state.observations.keys():
+            if observation == "DOLPHIN_SIGHTINGS":
+                if self.old_dolphins == -1:
+                    self.old_dolphins = trading_state.observations["DOLPHIN_SIGHTINGS"]
+                    continue
+                if trading_state.observations["DOLPHIN_SIGHTINGS"] - self.old_dolphins > 10:
+                    # print("DOLPHINS SPOTTED")
+                    self.dolphins_spotted = True
+                    self.dolphins_spotted_timestamp = trading_state.timestamp
+                if trading_state.observations["DOLPHIN_SIGHTINGS"] - self.old_dolphins < -10:
+                    # print("DOLPHINS GONE")
+                    self.dolphins_gone = True
+                    self.dolphins_gone_timestamp = trading_state.timestamp
+                self.old_dolphins = trading_state.observations["DOLPHIN_SIGHTINGS"]
+
+    def trade(self, trading_state: TradingState, orders: list):
+
+        self.handle_observations(trading_state)
+
+        order_depth: OrderDepth = trading_state.order_depths[self.name]
+
+        if self.dolphins_spotted and trading_state.timestamp - self.dolphins_spotted_timestamp < self.dolphin_action_time:
+            # start buying gear if dolphins have been spotted
+            print(self.dolphins_spotted_timestamp)
+            # print("BUYING GEAR")
+            print(trading_state.timestamp)
+            self.continuous_buy(order_depth, orders)
+
+        if self.dolphins_gone and trading_state.timestamp - self.dolphins_gone_timestamp < self.dolphin_action_time:
+            # start selling gear if dolphins are going away
+            # print("SELLING GEAR")
+            self.continuous_sell(order_depth, orders)
+        if self.dolphins_spotted and trading_state.timestamp - self.dolphins_spotted_timestamp > self.gear_timestamp_diff:
+            # Start selling after dolphins have been spotted for long enough
+            self.continuous_sell(order_depth, orders)
+
+            if trading_state.timestamp - self.dolphins_spotted_timestamp - self.gear_timestamp_diff > self.dolphin_action_time:
+                self.dolphins_spotted = False
+
+        if self.dolphins_gone and trading_state.timestamp - self.dolphins_gone_timestamp > self.gear_timestamp_diff:
+            # Start buying after dolphins have been gone for long enough
+            self.continuous_buy(order_depth, orders)
+            if trading_state.timestamp - self.dolphins_gone_timestamp - self.gear_timestamp_diff > self.dolphin_action_time:
+                self.dolphins_gone = False
+
+class TimeBasedStrategy(CrossStrategy):
+    def __init__(self, name, min_req_price_difference, max_position):
+        super().__init__(name, min_req_price_difference, max_position)
+        self.berries_ripe_timestamp = 350000
+        self.berries_peak_timestamp = 500000
+        self.berries_sour_timestamp = 650000
+
+    def trade(self, trading_state: TradingState, orders: list):
+        order_depth = trading_state.order_depths[self.name]
+        if 0 < trading_state.timestamp - self.berries_ripe_timestamp < 5000:
+            # print("BERRIES ALMOST RIPE")
+
+            # start buying berries if they start being ripe
+            if len(order_depth.sell_orders) != 0:
+                best_asks = sorted(order_depth.sell_orders.keys())
+
+                i = 0
+                while i < self.trade_count and len(best_asks) > i:
+                    if self.prod_position == -self.max_pos:
+                        break
+                    self.buy_product(best_asks, i, order_depth, orders)
+                    i += 1
+
+        elif 0 < trading_state.timestamp - self.berries_peak_timestamp < 5000:
+            # print("BERRIES READY TO SELL")
+            self.continuous_sell(order_depth, orders)
+        else:
+            super().trade(trading_state, orders)
+
+
+"""
+Products
+"""
+# 2024 Products
+
+# Tutorial Products
+class Amethysts(FixedStrategy):
+    def __init__(self):
+        super().__init__("AMETHYSTS", max_pos=20)
+
+class Starfruit(CrossStrategy):
+    def __init__(self):
+        super().__init__("STARFRUIT", min_req_price_difference=3, max_position=20)
+
+"""
+2023 Products
+class Pearls(FixedStrategy):
+    def __init__(self):
+        super().__init__("PEARLS", max_pos=20)
+
+
+class Bananas(CrossStrategy):
+    def __init__(self):
+        super().__init__("BANANAS", min_req_price_difference=3, max_position=20)
+
+
+class PinaColadas(DiffStrategy):
+    def __init__(self):
+        super().__init__("PINA_COLADAS", max_position=150, derivative_resolution=150, diff_thresh=30)
+
+
+class Baguette(DiffStrategy):
+    def __init__(self):
+        super().__init__("BAGUETTE", max_position=150, derivative_resolution=20, diff_thresh=200)
+
+
+class Dip(DiffStrategy):
+    def __init__(self):
+        super().__init__("DIP", max_position=300, derivative_resolution=100, diff_thresh=40)
+
+
+class Coconut(DiffStrategy):
+    def __init__(self):
+        super().__init__("COCONUTS", max_position=600, derivative_resolution=1500, diff_thresh=30)
+
+
+class Ukulele(DiffStrategy):
+    def __init__(self):
+        super().__init__("UKULELE", max_position=70, derivative_resolution=20, diff_thresh=150)
+
+
+class Basket(DiffStrategy):
+    def __init__(self):
+        super().__init__("PICNIC_BASKET", max_position=70, derivative_resolution=50, diff_thresh=100)
+
+
+class Berries(TimeBasedStrategy):
+    def __init__(self):
+        super().__init__("BERRIES", min_req_price_difference=2, max_position=250)
+
+
+class DivingGear(DiffStrategy):
+    def __init__(self):
+        super().__init__("DIVING_GEAR", max_position=50, derivative_resolution=15, diff_thresh=25)
+"""
+
+
+"""
+    Trader Logic
+"""
+AMETHYST_PRICE = 10000
+
+class Trader:
+    def __init__(self):
+        self.products = {
+            "AMETHYSTS": Amethysts(),
+            "STARFRUIT": Starfruit(),
+        }
+
+    def run(self, state: TradingState):
+        """
+        Only method required. It takes all buy and sell orders for all symbols as an input,
+        and outputs a list of orders to be sent
+        """
+
+        # Initialize the method output dict as an empty dict
+        result = {}
+
+        print(state)
+        print("traderData: " + state.traderData)
+        print("Observations: " + str(state.observations))
+        for product in state.order_depths.keys():
+            print("Product: ", product)
+            if product in self.products.keys():
+                orders: list[Order] = []
+
+                self.products[product].reset_from_state(state)
+                self.products[product].trade(trading_state=state, orders=orders)
+                result[product] = orders
+
+        # Sample conversion request. Check more details below in the programming
+        conversions = 1
+
+        # Serialize the trader state to JSON or any format that suits your need
+        traderData = json.dumps({
+            # Include any trader state data you want to preserve across executions
+            "someStateData": "SAMPLE"
+        })
+
+        return result, conversions, traderData
